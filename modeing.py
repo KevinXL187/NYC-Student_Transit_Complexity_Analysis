946498e565a88c8ba3bbd0579c7926d06008c5c2
Kevin Liu <xiongl.qixing@gmail.com>
2026-04-08 14:23:18 -0400

    feat: added basic modeling

--- modeing.py
+++ modeing.py
@@ -1,1 +1,16 @@
 # %%
+import statsmodels.api as sm
+
+# %%
+
+data = 
+
+# define independent variables (X) and dependent variable (y)
+X = data[['CCI', 'funding_per_pupil', 'pct_temp_housing']]
+y = data['Graduation_Rate']
+
+# model fitting
+model = sm.OLS(y, X).fit()
+
+# result
+print(model.summary())
