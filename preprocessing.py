afb4c8292e2d4b91d2972ac002bfb7016cd5e040
Kevin Liu <xiongl.qixing@gmail.com>
2026-04-09 15:09:00 -0400

    feat: finialized school dir processing function

--- preprocessing.py
+++ preprocessing.py
@@ -11,8 +11,6 @@ def convert_coords(row):
         return pd.Series({'lon': None, 'lat': None})
 
 def process_schools(input_path, output_path):
-    print(f"Processing schools from {input_path}...")
-    
     sch_df = pd.read_csv(input_path)
     
     coords = sch_df.apply(convert_coords, axis=1)
@@ -21,15 +19,19 @@ def process_schools(input_path, output_path):
 
     output_cols = [
         'LOCATION_CODE', 'LOCATION_NAME', 'lon', 'lat', 
-        'LOCATION_TYPE_DESCRIPTION', 'NTA_NAME'
+        'LOCATION_TYPE_DESCRIPTION', 'NTA_NAME', 'LOCATION_CATEGORY_DESCRIPTION'
     ]
     clean_df = sch_df[output_cols].copy()
+    clean_df['NTA_NAME'] = clean_df['NTA_NAME'].str.strip()
+    #print(clean_df['LOCATION_CATEGORY_DESCRIPTION'].value_counts())
+    clean_df = clean_df[(clean_df['LOCATION_CATEGORY_DESCRIPTION']  == 'High school') | 
+                        (clean_df['LOCATION_CATEGORY_DESCRIPTION']  == 'K-12 all grades')]
+    clean_df = clean_df.drop(columns=['LOCATION_CATEGORY_DESCRIPTION'])
 
     clean_df.to_csv(output_path, index=False)
 
 if __name__ == "__main__":
-    # Update these paths to match your project structure
-    raw_csv = "./data/spatial/School_Directory_2015.csv"
+    raw_csv = "./data/spatial/school_points_15.csv"
     processed_csv = "processed_schools_2015.csv"
 
     process_schools(raw_csv, processed_csv)
