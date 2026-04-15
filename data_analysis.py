2ce138f66f867a4c8a6fc9716661d626a4182b18
Kevin Liu <xiongl.qixing@gmail.com>
2026-04-15 12:25:23 -0400

    feat: add code to load data structures

--- data_analysis.py
+++ data_analysis.py
@@ -2,12 +2,11 @@
 # pyright: basic
 import os
 import networkx as nx
+import geopandas as gpd
 
-# %%
-# calculate Commute Complexity Index formula function
 
 def calculate_CCI(G, origin, dest):
-
+    # calculate commute complexity index formula
     path = nx.shortest_path(G, source=origin, target=dest, weight='weight')
     total_travel_time = nx.shortest_path_length(G, origin, dest, weight='weight')
         
@@ -29,3 +28,12 @@ def calculate_CCI(G, origin, dest):
     cci = (total_travel_time / 60) * (1 + (transfer_count * transfer_penalty))
         
     return round(cci, 2)
+
+if __name__ == "__main__":
+
+    # loading data
+    with open('my_graph.pkl', 'rb') as f:
+        nx_graph = pickle.load(f)
+
+    gdf_edges = gpd.read_file('network_data.gpkg', layers='edges')
+    gdf_nodes = gpd.read_file('network_data.gpkg', layers='nodes')
