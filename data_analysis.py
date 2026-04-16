1161e0d06cb5bc2ea0392497082e6b7eef867771
Kevin Liu <xiongl.qixiong@gmail.com>
2026-04-16 14:39:02 -0400

    feat: added cci formula function

--- data_analysis.py
+++ data_analysis.py
@@ -1,10 +1,13 @@
 # pyright: basic
-import os
+import os, pickle
 import networkx as nx
 import geopandas as gpd
+import numpy as np
 
 
 def calculate_CCI(nx_graph, origin, dest):
+    transfer_penalty = 300
+    results = []
     nta_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'origin']
     school_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'school']
 
@@ -12,11 +15,34 @@ def calculate_CCI(nx_graph, origin, dest):
     if not nx.is_connected(nx_graph.to_undirected()):
         print("Warning - graph has isolated islands and some schools will be unreachable")
 
-    for start_nodes in nta_nodes:
-        distances = nx.single_source_dijkstra_path_length(nxG_final, start_nta, weight='weight')
-        
-        for target_nodes in school_nodes:
-            travel_times = distances.get(target_nodes, np.nan)
+    for start_node in nta_nodes:
+        distances, paths = nx.single_source_dijkstra_path_length(nx_graph, start_node, weight='weight')
+        nta_cci_score = 0
+        reachable_qualities = []
+
+        for target in school_nodes:
+            if target in paths:
+
+                path = paths[target]
+                time = distances[target]
+                num_transfers = 0
+                prev_relation = None
+                
+                # Step through the path edges to count mode switches
+                for i in range(len(path) - 1):
+                    u, v = path[i], path[i+1]
+                    edge_data = nx_graph.get_edge_data(u, v)
+                    rel = edge_data[0].get('relation') if isinstance(edge_data, dict) and 0 in edge_data else edge_data.get('relation')
+                    
+                    if prev_relation and rel != prev_relation:
+                        if 'walking' in prev_relation and 'transit' in rel:
+                            num_transfers += 1 # Walk -> Bus/Subway
+                        elif 'transit' in prev_relation and 'transit' in rel:
+                            num_transfers += 1 # Bus -> Subway transfer
+                    
+                    prev_relation = rel
+
+            travel_times = distances.get(target, np.nan)
 
             sch_
 
