432c97509132074dddbc3957794736c61cb08f5b
Kevin Liu <xiongl.qixing@gmail.com>
2026-04-23 20:22:11 -0400

    feat: merge two function into one with one bool variable

--- data_analysis.py
+++ data_analysis.py
@@ -6,72 +6,36 @@ import pandas as pd
 import numpy as np
 
 
-def calculate_CCI_with_Tpenalty(nx_graph):
-    transfer_penalty = 300  # Penalty in seconds/units
-    results = {}
-    
-    nta_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'origin']
-    school_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'school']
+def calculate_CCI_with_Tpenalty(nx_graph, apply_penalty=True):
+    transfer_penalty = 180
 
-    if not nx.is_connected(nx_graph.to_undirected()):
-        print("Warning - graph has isolated islands and some schools will be unreachable")
-
-    for start_node in nta_nodes:
-        # Get both distances (weights) and the actual paths to calculate transfers
-        distances, paths = nx.single_source_dijkstra(nx_graph, start_node, weight='weight')
-        school_scores = {}
-
-        for target in school_nodes:
-            if target in paths:
-                path = paths[target]
-                base_time = distances[target]
-                num_transfers = 0
-                prev_relation = None
-                
-                for i in range(len(path) - 1):
-                    u, v = path[i], path[i+1]
-                    
-                    # For MultiDiGraph, get the edge with the minimum weight or first available
-                    edge_data = nx_graph.get_edge_data(u, v)
-                    if isinstance(edge_data, dict): data = edge_data[0] 
-                    else:   data = edge_data
-                    
-                    rel = data.get('relation', '')
-                    
-                    if prev_relation and rel != prev_relation:
-                        # Logic: Transfer occurs when switching modes or transit lines
-                        if ('walking' in prev_relation and 'transit' in rel) or \
-                           ('transit' in prev_relation and 'transit' in rel):
-                            num_transfers += 1
-                    
-                    prev_relation = rel
-                
-                # CCI = Time + (Transfers * Penalty)
-                school_scores[target] = base_time + (num_transfers * transfer_penalty)
-            else:   school_scores[target] = np.nan
+    for u, v, k, data in nx_graph.edges(keys=True, data=True):
+        base_time = data.get('weight', 0)
+        penalty = 0
         
-        results[start_node] = school_scores
-    
-    return results
-
-def calculate_CCI_no_Tpenalty(nx_graph):
-    if not nx.is_connected(nx_graph.to_undirected()):
-        print("Warning - graph has isolated islands and some schools will be unreachable")
+        if apply_penalty:
+            rel = data.get('relation', '')
+            if rel == 'walk_transit':
+                penalty = penalty_val * 1.75
+            elif rel == 'sub_transfer':
+                penalty = penalty_val * 1.25
+        
+        data['tmp_w'] = base_time + penalty
 
     nta_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'origin']
-    school_nodes = [n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'school']
+    school_nodes = {n for n, d in nx_graph.nodes(data=True) if d.get('type') == 'school'}
 
     results = {}
-
     for start_node in nta_nodes:
-        distances = nx.single_source_dijkstra_path_length(nx_graph, start_node, weight='weight')
-        school_score = {}
-        for target_node in school_nodes:
-            travel_time = distances.get(target_node, np.nan)
-            school_score[target_node] = travel_time
-        results[start_node] = school_score
-    
+        if start_node not in nx_graph:
+            results[start_node] = {s: np.nan for s in school_nodes}
+            continue
+        lengths = nx.single_source_dijkstra_path_length(nx_graph, start_node, weight='tmp_w')
+
+        results[start_node] = {s: lengths.get(s, np.nan) for s in school_nodes}
+        
     return results
+
         
 def CCI_graph(nx_graph, results):
     flattened_data = []
